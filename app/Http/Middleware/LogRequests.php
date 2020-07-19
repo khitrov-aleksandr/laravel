aa8cbfc4d38d4b7212dd2fb8ce49cde2772f9958
Хитров Александр <khitrov.aleksandr@gmail.com>
2020-07-19 23:02:06 +0300

    Игнорирование ошибок записи в лог

--- app/Http/Middleware/LogRequests.php
+++ app/Http/Middleware/LogRequests.php
@@ -8,18 +8,23 @@ use Illuminate\Support\Facades\Log;
 class LogRequests
 {
     const CHANNEL = 'request';
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        Log::channel(self::CHANNEL)->info('Request path: ' . $request->path());
-        Log::channel(self::CHANNEL)->info('Request method: ' . $request->method());
-        Log::channel(self::CHANNEL)->info('Request body: ' . "\n" . json_encode($request->all(), JSON_PRETTY_PRINT));
+        try {
+            Log::channel(self::CHANNEL)->info('Request path: ' . $request->path());
+            Log::channel(self::CHANNEL)->info('Request method: ' . $request->method());
+            Log::channel(self::CHANNEL)->info('Request body: ' . "\n" . json_encode($request->all(), JSON_PRETTY_PRINT));
+        } catch (\Exception $e) {
+            //Still empty. Ignore write errors if no free disk space for example
+        }
         return $next($request);
     }
 }
