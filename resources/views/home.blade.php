8f4ea90af8dc944f398f3e36be05c0ec07f7c72c
Хитров Александр <khitrov.aleksandr@gmail.com>
2021-01-17 22:34:10 +0300

    Change header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello it's Home Page</h1>
+    <h1>Leaflet and OpenStreetMap React component</h1>
     <div id="map-component"></div>
 @endsection
