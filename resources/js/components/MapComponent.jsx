24169ef76022dc3839fc355f16ae96ecb54b2b7f
Хитров Александр <khitrov.aleksandr@gmail.com>
2021-01-17 11:10:28 +0300

    Develop map

--- resources/js/components/MapComponent.jsx
+++ resources/js/components/MapComponent.jsx
@@ -4,38 +4,51 @@ import L from 'leaflet';
 
 class MapComponent extends React.Component {
     leafLet() {
-
         var zoom = 13;
         var center = [54.194946, 37.620137];
-        var point = [54.209370, 37.628065];
+        var points = [
+            ['Адрес 1', 'Подробный адрес 1', 54.209370, 37.628065],
+            ['Адрес 2', 'Подробный адрес 2', 54.191863, 37.621390]
+        ];
 
         var map = L.map('mapid').setView(center, zoom);
 
-        var greenIcon = L.icon({
+        var blueIcon = L.icon({
             iconUrl: 'images/vendor/leaflet/dist/marker-icon.png',
             shadowUrl: 'images/vendor/leaflet/dist/marker-shadow.png',
             //iconSize:     [38, 95], // size of the icon
             //shadowSize:   [50, 64], // size of the shadow
-            iconAnchor:   [20, 15], // point of the icon which will correspond to marker's location
+            iconAnchor: [20, 15], // point of the icon which will correspond to marker's location
             shadowAnchor: [20, 15],  // the same for the shadow
-            popupAnchor:  [-10, -10] // point from which the popup should open relative to the iconAnchor
+            popupAnchor: [-10, -10] // point from which the popup should open relative to the iconAnchor
         });
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        var marker = L.marker(point, {icon: greenIcon}).addTo(map)
-            .bindPopup('<b>RifeBank</b><br>Banking address')
-            .openPopup();
+        var markers = [];
+        var clearPoints = [];
+        for (var i = 0; i < points.length; i++) {
 
-        var group = new L.featureGroup([marker]);
+            var point = [points[i][2], points[i][3]];
+
+            var marker = L.marker(point, {icon: blueIcon})
+                .bindPopup('<b>' + points[i][0] + '</b><br>' + points[i][1], {autoClose:false})
+                .addTo(map)
+                .openPopup();
+
+            clearPoints.push(point)
+            markers.push(marker)
+        }
+
+        var group = new L.featureGroup(markers);
 
         //Auto zoom
         map.fitBounds(group.getBounds());
 
         //Auto center
-        map.fitBounds([point]);
+        map.fitBounds(clearPoints);
     }
 
     componentDidMount() {
